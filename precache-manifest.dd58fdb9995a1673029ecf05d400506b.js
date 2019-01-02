c9bd209f2f9f0a7babe9fc26d03c2d2d8e55e594
Steve Vander Stouw <svanderstouw@gmail.com>
2019-01-02 15:11:46 -0500

    Updates

---
+++ precache-manifest.dd58fdb9995a1673029ecf05d400506b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "340a140dc48e109e29a4",
+    "url": "/meme-generator/static/js/runtime~main.340a140d.js"
+  },
+  {
+    "revision": "1f0d582700f365e6a41d",
+    "url": "/meme-generator/static/js/main.1f0d5827.chunk.js"
+  },
+  {
+    "revision": "5cf7f9fd534872cb7ca7",
+    "url": "/meme-generator/static/js/1.5cf7f9fd.chunk.js"
+  },
+  {
+    "revision": "1f0d582700f365e6a41d",
+    "url": "/meme-generator/static/css/main.d5461860.chunk.css"
+  },
+  {
+    "revision": "0ef71ad475f785bc7f9023ff26d19d3b",
+    "url": "/meme-generator/index.html"
+  }
+];
